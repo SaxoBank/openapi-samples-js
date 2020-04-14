797ca9f9f7d8ff58d1159f1162c6c537baf303ef
bas <post@teach2000.org>
2020-04-14 13:01:11 +0200

    Example on formatting prices

---
+++ instruments/display-positions-orders/README.md
@@ -0,0 +1,5 @@
+# Client-side Sample for displaying the instruments in the portfolio
+
+This is a demonstration on how to format numbers in the portfolio.
+
+Live demo: https://saxobank.github.io/openapi-samples-js/portfolio/
---
+++ instruments/display-positions-orders/boilerplate.js
@@ -0,0 +1,217 @@
+/*jslint this: true, browser: true, for: true, long: true */
+/*global console URLSearchParams */
+
+const apiUrl = "https://gateway.saxobank.com/sim/openapi";
+let accountKey = "";
+let clientKey = "";
+const responseElm = document.getElementById("idResponse");
+const accessTokenElm = document.getElementById("idBearerToken");
+
+/**
+ * Determine if the token edit exists.
+ * @return {boolean} True if the field exists.
+ */
+function tokenInputFieldExists() {
+    return accessTokenElm !== null;
+}
+
+/**
+ * Shared function to display an unsuccessful response.
+ * @param {Object} errorObject The complete error object.
+ * @return {void}
+ */
+function processError(errorObject) {
+    let textToDisplay = "Error with status " + errorObject.status + " " + errorObject.statusText;
+    // Some errors have a JSON-response, containing explanation of what went wrong.
+    errorObject.json().then(function (errorObjectJson) {
+        if (errorObjectJson.hasOwnProperty("ErrorInfo")) {
+            // Order error object is wrapped in ErrorInfo
+            errorObjectJson = errorObjectJson.ErrorInfo;
+        }
+        // Order preview error object not..
+        if (errorObjectJson.hasOwnProperty("ErrorCode")) {
+            textToDisplay += " - " + errorObjectJson.ErrorCode + " (" + errorObjectJson.Message + ")";
+        }
+        console.error(textToDisplay);
+    }).catch(function () {
+        // Typically 401 (Unauthorized) has an empty response, this generates a SyntaxError.
+        console.error(textToDisplay);
+    });
+}
+
+/**
+ * Show a function and run it.
+ * @param {Function} functionToRun The function in scope.
+ * @return {void}
+ */
+function run(functionToRun) {
+
+    function getAllAccounts(header) {
+        fetch(apiUrl + "/port/v1/accounts/me", header).then(function (response) {
+            if (response.ok) {
+                response.json().then(function (responseJson) {
+                    const cbxAccount = document.getElementById("idCbxAccount");
+                    let i;
+                    let option;
+                    for (i = cbxAccount.options.length - 1; i >= 0; i -= 1) {
+                        cbxAccount.remove(i);
+                    }
+                    for (i = 0; i < responseJson.Data.length; i += 1) {
+                        option = document.createElement("option");
+                        option.text = responseJson.Data[i].AccountId + " (" + responseJson.Data[i].AccountType + ", " + responseJson.Data[i].Currency + ")";
+                        option.value = responseJson.Data[i].AccountKey;
+                        if (option.value === accountKey) {
+                            option.setAttribute("selected", true);
+                        }
+                        cbxAccount.add(option);
+                    }
+                    cbxAccount.addEventListener("change", function () {
+                        accountKey = cbxAccount.value;
+                        responseElm.innerText = "Using account " + accountKey;
+                    });
+                    functionToRun();
+                });
+            } else {
+                processError(response);
+            }
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }
+
+    function getDefaultAccount(header) {
+        fetch(apiUrl + "/port/v1/clients/me", header).then(function (response) {
+            if (response.ok) {
+                accessTokenElm.setCustomValidity("");
+                response.json().then(function (responseJson) {
+                    accountKey = responseJson.DefaultAccountKey;  // Remember the default account
+                    clientKey = responseJson.ClientKey;
+                    responseElm.innerText = "The token is valid - hello " + responseJson.Name + "\nClientKey: " + clientKey;
+                    getAllAccounts(header);
+                });
+            } else {
+                accessTokenElm.setCustomValidity("Invalid access_token.");
+                processError(response);
+            }
+        }).catch(function (error) {
+            accessTokenElm.setCustomValidity("Invalid access_token.");
+            console.error(error);
+        });
+    }
+
+    // Display source of function, for demonstration:
+    document.getElementById("idJavaScript").innerText = functionToRun.toString();
+    responseElm.removeAttribute("style");
+    responseElm.innerText = "Started function " + functionToRun.name + "()..";
+    if (tokenInputFieldExists()) {
+        if (accessTokenElm.value.length < 10) {
+            accessTokenElm.setCustomValidity("Bearer token is required for requests.");
+            console.error("Bearer token is required for requests.");
+        } else {
+            if (accountKey === "") {
+                // Retrieve the account key first
+                getDefaultAccount({
+                    "headers": {
+                        "Content-Type": "application/json; charset=utf-8",
+                        "Authorization": "Bearer " + accessTokenElm.value
+                    },
+                    "method": "GET"
+                });
+            } else {
+                functionToRun();
+            }
+        }
+    } else {
+        functionToRun();
+    }
+}
+
+(function () {
+
+    /**
+     * When an error is logged to the console, show it in the Response-box as well.
+     * @return {void}
+     */
+    function mirrorConsoleError() {
+        console.errorCopy = console.error.bind(console);
+        console.error = function (data) {
+            responseElm.setAttribute("style", "background-color: #e10c02; color: #ffffff;");
+            responseElm.innerText = data;
+            this.errorCopy(data);
+        };
+    }
+
+    /**
+     * When something is logged to the console, show it in the Response-box as well.
+     * @return {void}
+     */
+    function mirrorConsoleLog() {
+        console.logCopy = console.log.bind(console);
+        console.log = function (data) {
+            responseElm.innerText = data;
+            this.logCopy(data);
+        };
+    }
+
+    /**
+     * Read a cookie.
+     * @param {string} key Name of the cookie.
+     * @return {string} Value.
+     */
+    function getCookie(key) {
+        const name = key + "=";
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const cookieArray = decodedCookie.split(";");
+        let c;
+        let i;
+        for (i = 0; i < cookieArray.length; i += 1) {
+            c = cookieArray[i];
+            while (c.charAt(0) === " ") {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+    /**
+     * Try to hunt down a previously used access_token, so a page refresh is less a hassle.
+     * @return {void}
+     */
+    function tryToGetToken() {
+        // First, maybe the token is supplied in the URL?
+        const urlParams = new URLSearchParams(window.location.hash.replace("#", "?"));
+        const urlWithoutParams = location.protocol + "//" + location.host + location.pathname;
+        let newAccessToken = urlParams.get("access_token");
+        if (newAccessToken === null) {
+            // Second, maybe the token is stored in a cookie?
+            newAccessToken = getCookie("saxotoken");
+        }
+        accessTokenElm.value = newAccessToken;
+        window.addEventListener("beforeunload", function () {
+            let accessTokenToSave = accessTokenElm.value;
+            if (accessTokenToSave.length > 10) {
+                // Save the token as session cookie, so it can be reused:
+                document.cookie = "saxotoken=" + accessTokenToSave;
+            }
+        });
+        document.getElementById("idBtnValidate").addEventListener("click", function () {
+            accountKey = "";
+            run(function () {
+                // Token is valid!
+            });
+        });
+        if (urlWithoutParams.substring(0, 36) === "http://localhost/openapi-samples-js/" || urlWithoutParams.substring(0, 46) === "https://saxobank.github.io/openapi-samples-js/") {
+            // We can probably use the Implicit Grant to get a token
+            document.getElementById("idHrefRetrieveToken").href = "https://sim.logonvalidation.net/authorize?client_id=e081be34791f4c7eac479b769b96d623&response_type=token&redirect_uri=" + encodeURIComponent(urlWithoutParams);
+        }
+    }
+    mirrorConsoleLog();
+    mirrorConsoleError();
+    if (tokenInputFieldExists()) {
+        tryToGetToken();
+    }
+
+}());
