3f3638819f92e81df9b6d9c1435d473180c9aa10
bas <post@teach2000.org>
2020-05-14 21:36:36 +0200

    Ability to find futures space

--- instruments/instrument-search/demo.js
+++ instruments/instrument-search/demo.js
@@ -2,6 +2,7 @@
 /*global window console accountKey run processError displayVersion apiUrl */
 
 let instrumentId;
+let instrumentIdType;
 
 /**
  * This is an example of getting all exchanges.
@@ -95,17 +96,25 @@ function getLegalAssetTypes(callback) {
  * @return {void}
  */
 function findInstrument() {
-    const keywords = encodeURIComponent(document.getElementById("idInstrumentName").value);
+    const assetType = document.getElementById("idCbxAssetType").value;
+    const keywords = document.getElementById("idInstrumentName").value + (
+        assetType === "ContractFutures"
+        ? " continuous"  // By adding this, non tradable FuturesSpaces can be found
+        : ""
+    );
     let url;
-    if (document.getElementById("idCbxAssetType").value === "-") {
+    if (assetType === "-") {
         getLegalAssetTypes(findInstrument);
     } else {
-        url = apiUrl + "/ref/v1/instruments?AssetTypes=" + document.getElementById("idCbxAssetType").value + "&$top=5" + "&AccountKey=" + encodeURIComponent(accountKey) + "&Keywords=" + keywords;
+        url = apiUrl + "/ref/v1/instruments?AssetTypes=" + assetType + "&$top=10" + "&AccountKey=" + encodeURIComponent(accountKey) + "&Keywords=" + encodeURIComponent(keywords);
         if (document.getElementById("idCbxExchange").value !== "-") {
             url += "&ExchangeId=" + encodeURIComponent(document.getElementById("idCbxExchange").value);
         }
         if (document.getElementById("idChkMultiLeg").checked) {
             url += "&CanParticipateInMultiLegOrder=" + true;
+        }
+        if (assetType === "ContractFutures") {
+            url += "&IncludeNonTradable=true";  // This way you'll find the FuturesSpaces, using keyword "continuous"
         }
         fetch(
             url,
@@ -119,14 +128,23 @@ function findInstrument() {
         ).then(function (response) {
             if (response.ok) {
                 response.json().then(function (responseJson) {
+                    let isInstrumentTradable;
                     if (responseJson.Data.length > 0) {
                         // Remember the first Uic for the details request
-                        if (responseJson.Data[0].hasOwnProperty("PrimaryListing")) {
+                        if (responseJson.Data[0].hasOwnProperty("PrimaryListing") && assetType === "Stock") {
                             // Stocks might have a primary listing on another market
                             instrumentId = responseJson.Data[0].PrimaryListing;
                         } else {
                             // This is not called "Uic", because is can identify an OptionRoot as well
                             instrumentId = responseJson.Data[0].Identifier;
+                        }
+                        isInstrumentTradable = responseJson.Data[0].hasOwnProperty("TradableAs");
+                        if (!isInstrumentTradable && assetType === "ContractFutures") {
+                            instrumentIdType = "futuresSpace";
+                        } else if (assetType === "StockOption" || assetType === "FuturesOption" || assetType === "StockIndexOption") {
+                            instrumentIdType = "optionRoot";
+                        } else {
+                            instrumentIdType = "uic";
                         }
                     }
                     console.log(JSON.stringify(responseJson, null, 4));
@@ -147,13 +165,15 @@ function findInstrument() {
 function getDetails() {
     const assetType = document.getElementById("idCbxAssetType").value;
     let urlPath;
-    let isOptionRoot = false;
-    if (assetType === "StockOption" || assetType === "FuturesOption" || assetType === "StockIndexOption") {
-        // This instrumentId is not a Uic, but an option root. Series can be retrieved.
-        isOptionRoot = true;
+    switch (instrumentIdType) {
+    case "optionRoot":  // This identifier is not a Uic, but an option root. Series can be retrieved.
         urlPath = "/ref/v1/instruments/contractoptionspaces/" + instrumentId;
-    } else {
-        urlPath = "/ref/v1/instruments/details?Uics=" + instrumentId + "&AssetTypes=" + assetType + "&AccountKey=" + encodeURIComponent(accountKey);
+        break;
+    case "futuresSpace":  // This identifier is not a Uic, but a futures space.
+        urlPath = "/ref/v1/instruments/futuresspaces/" + instrumentId;
+        break;
+    default:
+        urlPath = "/ref/v1/instruments/details/" + instrumentId + "/" + assetType + "?AccountKey=" + encodeURIComponent(accountKey);
     }
     fetch(
         apiUrl + urlPath,
@@ -167,10 +187,15 @@ function getDetails() {
     ).then(function (response) {
         if (response.ok) {
             response.json().then(function (responseJson) {
-                if (isOptionRoot) {
-                    console.log("The search result contained an option root. These are the series with their Uics:\n\n" + JSON.stringify(responseJson, null, 4));
-                } else {
-                    console.log("These are the details of this instrument:\n\n" + JSON.stringify(responseJson.Data, null, 4));
+                switch (instrumentIdType) {
+                case "optionRoot":
+                    console.log("The search result contained an option root (# " + instrumentId + "). These are the series with their Uics:\n\n" + JSON.stringify(responseJson, null, 4));
+                    break;
+                case "futuresSpace":
+                    console.log("The search result contained a futures space (# " + instrumentId + "). These are the futures in this space, with their Uics:\n\n" + JSON.stringify(responseJson, null, 4));
+                    break;
+                default:
+                    console.log("These are the details of this instrument:\n\n" + JSON.stringify(responseJson, null, 4));
                 }
             });
         } else {
---
+++ orders/futures/README.md
@@ -0,0 +1,5 @@
+# Client-side Samples for placing Option Orders
+
+This is a demonstration on how to place an option order with different conditions (durations and order types).
+
+Live demo: https://saxobank.github.io/openapi-samples-js/orders/options/
