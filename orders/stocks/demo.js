bb04f8e7cae67b786970e4845d8f0bdfef703002
bas <post@teach2000.org>
2020-06-16 10:11:04 +0200

    Highlight error of PreCheck

--- orders/stocks/demo.js
+++ orders/stocks/demo.js
@@ -337,7 +337,7 @@
                         // Order could be placed if the account had sufficient margin and funding.
                         // In this case all calculated cost and margin values are in the response, together with an ErrorInfo object:
                         if (responseJson.hasOwnProperty("ErrorInfo")) {
-                            console.error(JSON.stringify(responseJson, null, 4));
+                            console.error(response.ErrorInfo.Message + "\n\n" + JSON.stringify(responseJson, null, 4));
                         } else {
                             // The order can be placed
                             console.log(JSON.stringify(responseJson, null, 4));
