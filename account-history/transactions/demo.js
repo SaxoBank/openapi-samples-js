ad7d5c1b1e503597bfc3b956aeca1aa261b34a92
ahen <10469240+afmhenry@users.noreply.github.com>
2022-04-01 16:44:35 +0200

    removed irrelevant copied functions

--- account-history/transactions/demo.js
+++ account-history/transactions/demo.js
@@ -59,80 +59,14 @@
             console.error(error);
         });
     }
+//this should be good inspiration for looping over response!
 
-    /**
-     * Example of getting options for the transfer.
-     * @return {void}
-     */
-    function getTransactionsForClient(start_date, end_date) {
-        fetch(
-            demo.apiUrl + "/atr/v1/cashmanagement/beneficiaryinstructions?ClientKey=" + encodeURIComponent(demo.user.clientKey),
-            {
-                "method": "GET",
-                "headers": {
-                    "Authorization": "Bearer " + document.getElementById("idBearerToken").value
-                }
-            }
-        ).then(function (response) {
-            if (response.ok) {
-                response.json().then(function (responseJson) {
-                    const cbxBeneficiaryAccount = document.getElementById("idCbxBeneficiaryAccount");
-                    let i;
-                    let option;
-                    for (i = cbxBeneficiaryAccount.options.length - 1; i >= 0; i -= 1) {
-                        cbxBeneficiaryAccount.remove(i);
-                    }
-                    for (i = 0; i < responseJson.Data.length; i += 1) {
-                        option = document.createElement("option");
-                        option.text = responseJson.Data[i].BeneficiaryDetails.AccountNumber + " (" + responseJson.Data[i].Currency + ") " + responseJson.Data[i].Name;
-                        option.value = responseJson.Data[i].BeneficiaryInstructionId;
-                        cbxBeneficiaryAccount.add(option);
-                    }
-                    console.log("Result: " + JSON.stringify(responseJson, null, 4));
-                });
-            } else {
-                demo.processError(response);
-            }
-        }).catch(function (error) {
-            console.error(error);
-        });
-    }
-
-    /**
-     * Example of requesting a money withdrawal.
-     * @return {void}
-     */
-    function transferMoney() {
-        let result = "Description contains " + document.getElementById("idEdtDescription").value.length + " characters\n\n";
-        fetch(
-            demo.apiUrl + "/atr/v1/cashmanagement/withdrawals",
-            {
-                "method": "POST",
-                "headers": {
-                    "Authorization": "Bearer " + document.getElementById("idBearerToken").value,
-                    "Content-Type": "application/json; charset=utf-8"
-                },
-                "body": JSON.stringify({
-                    "AccountKey": demo.user.accountKey,
-                    "BeneficiaryInstructionId": document.getElementById("idCbxBeneficiaryAccount").value,
-                    "Currency": document.getElementById("idEdtCurrency").value,
-                    "Amount": document.getElementById("idEdtAmount").value,
-                    "MessageToBeneficiary": document.getElementById("idEdtDescription").value
-                })
-            }
-        ).then(function (response) {
-            if (response.ok) {
-                response.json().then(function (responseJson) {
-                    result += "Response has code " + response.status + " " + response.statusText + ": " + JSON.stringify(responseJson, null, 4);
-                    console.log(result);
-                });
-            } else {
-                demo.processError(response);
-            }
-        }).catch(function (error) {
-            console.error(error);
-        });
-    }
+                    // for (i = 0; i < responseJson.Data.length; i += 1) {
+                    //     option = document.createElement("option");
+                    //     option.text = responseJson.Data[i].BeneficiaryDetails.AccountNumber + " (" + responseJson.Data[i].Currency + ") " + responseJson.Data[i].Name;
+                    //     option.value = responseJson.Data[i].BeneficiaryInstructionId;
+                    //     cbxBeneficiaryAccount.add(option);
+                    // }
 
     demo.setupEvents([
         {"evt": "change", "elmId": "idCbxAccount", "func": getAccountCurrency, "funcsToDisplay": [getAccountCurrency], "isDelayedRun": true},
